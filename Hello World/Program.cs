28af2ec9a2a50f56bd8f1132f17fc1e659a9eff1
vavijad873 <vavijad873@gmail.com>
2021-06-05 00:51:05 -0400

    Add line 2

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -7,6 +7,7 @@ namespace Hello_World
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This is line 2");
         }
     }
 }
