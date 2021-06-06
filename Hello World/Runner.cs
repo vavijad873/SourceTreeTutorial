561e3ffa4cb71aa46c19643724396bfbdad39a79
vavijad873 <vavijad873@gmail.com>
2021-06-06 01:24:56 -0400

    add division

--- Hello World/Runner.cs
+++ Hello World/Runner.cs
@@ -21,7 +21,7 @@ namespace Hello_World
 
             Console.WriteLine($"The difference between 5 and 7 is {calculator.Substract(5, 7)}");
             Console.WriteLine($"The quotient of 20 and 4 is {calculator.Division(20,4)}");
-
+            Console.WriteLine($"The product of 5 and 7 is {calculator.Multiply(5, 7)}");
         }
     }
 
